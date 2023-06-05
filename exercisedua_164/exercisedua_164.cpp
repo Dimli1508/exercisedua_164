717978f8313661e9aa518e4342b91d8a7743fbb8
Mashell0707 <merlsutama@gmail.com>
2023-06-05 14:56:15 +0700

    dahlaa

--- exercisedua_164/exercisedua_164.cpp
+++ exercisedua_164/exercisedua_164.cpp
@@ -1,35 +1,85 @@
 #include <iostream>
-#include <vector>
 #include <string>
-using namespace std; 
+#include <vector>
+using namespace std;
 
-class pengarang;
-class penerbit {
-private:
-    string nama_penerbit;
-    vector <pengarang> daftar_pengarang;
+class Penerbit {
 public:
-    penerbit(const string& nama) : nama_penerbit(nama) {}
-    void tambah_pengarang(pengarang* pengarang) {
-        daftar_pengarang.push_back(pengarang);
+    string nama;
+    vector<string> penulis;
+
+    Penerbit(string nama) {
+        nama = nama;
     }
-    void daftar_pengarang() {
-        cout << "Daftar pengarang pada penerbit\"" << nama_penerbit << "\":" << endl;
-        for (auto pengarang : daftar_pengarang) {
-            cout << pengarang->get_nama_pengarang() << endl;
-        }
-        cout << endl;
+};
+
+class Pengarang {
+public:
+    string nama;
+    vector<string> buku;
+    vector<string> penerbit;
+
+    Pengarang(string nama) {
+        nama = nama;
     }
 };
 
 class buku {
-private:
-    string judul_buku;
 public:
-    buku(const string& judul) : judul_buku(judul) {}
-    string get_judul_buku() {
-        return judul_buku;
+    string nama;
+    vector<string> pengarang;
+
+    buku(string nama) {
+        nama = nama;
     }
 };
 
+int main() {
+    Pengarang joko("Joko");
+    Pengarang lia("Lia");
+    Pengarang giga("Giga");
+    Pengarang asroni("Asroni");
+    
+    Penerbit gamaPress("Gama Press");
+    Penerbit intanPariwara("Intan Pariwara");
 
+    joko.buku = { "Fisika", "Algoritma" };
+    lia.buku = { "Basisdata" };
+    giga.buku = { "Matematika", "Multimedia 1" };
+    asroni.buku = { "Dasar Pemrograman" };
+    giga.penerbit = { "Game Press", "Intan Pariwira" };
+    
+    gamaPress.penulis = { joko.nama, lia.nama, giga.nama };
+    intanPariwara.penulis = { asroni.nama, giga.nama };
+
+   
+    cout << "Daftar pengarang pada penerbit \"Gama Press\":" << endl;
+    for (const string& nama : gamaPress.penulis) {
+        cout << nama << endl;
+    }
+    cout << "Daftar pengarang pada penerbit \"Intan Pariwara\":" << endl;
+    for (const string& nama : intanPariwara.penulis) {
+        cout << nama << endl;
+    }
+    cout << "Daftar penerbit yang diikuti \"Giga\":" << endl;
+    for (const string& nama : giga.penerbit) {
+        cout << nama << endl;
+    }
+    cout << "Daftar buku yang dikarang \"Joko\":" << endl;
+    for (const string& buku : joko.buku) {
+        cout << buku << endl;
+    }
+    cout << "Daftar buku yang dikarang \"Lia\":" << endl;
+    for (const string& buku : lia.buku) {
+        cout << buku << endl;
+    }
+    cout << "Daftar buku yang dikarang \"Asroni\":" << endl;
+    for (const string& buku : asroni.buku) {
+        cout << buku << endl;
+    }
+    cout << "Daftar buku yang dikarang \"Giga\":" << endl;
+    for (const string& buku : giga.buku) {
+        cout << buku << endl;
+    }
+    return 0;
+}
